b27476bbef5e42eaab4c1b2458e2f4144f52ff5a
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-06-11 17:01:47 +0000

    0010_tensors.py: simple skeleton created

---
+++ 0010_tensors.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# vim: set foldmethod=marker:
+
+# {{{ imports
+
+import sys
+
+from logging import getLogger, basicConfig, INFO
+basicConfig(format='%(asctime)s [%(relativeCreated)7.0f] [%(levelname).1s] %(message)s',level=INFO,stream=sys.stderr)
+log = getLogger(__name__)
+
+from argparse import ArgumentParser
+
+# }}}
+# -------- TensorCreation(object) -- class --------
+# {{{ TensorCreation -- class
+
+class TensorCreation(object):
+
+# }}}
+# {{{ TensorCreation.__init__()
+
+    def __init__(self,args):
+        log.info("TensorCreation.__init__()")
+        self.__args = args
+
+# }}}
+# {{{ TensorCreation.run()
+
+    def run(self):
+        log.info("TensorCreation.run()")
+
+# }}}
+# -------- main --------
+# {{{ main
+
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        description = '0010_tensors.py (v1.0)',
+        epilog = "sfmunoz (C) 2026",
+    )
+
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help='enable debug mode')
+
+    args = parser.parse_args()
+
+    if args.debug:
+        from logging import DEBUG
+        log.setLevel(DEBUG)
+
+    TensorCreation(args).run()
+
+# }}}
